cdeb20c417255d582bc52fad4db6f4ab7990f53b
Sergio Bejarano <sergio@wilbby.com>
2024-07-27 20:36:45 +0200

    add firebase hosting

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
+import { initializeApp } from "firebase/app";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,6 +17,16 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const firebaseConfig = {
+    apiKey: "AIzaSyDObNpqRLzM2wAE3HjD_wAwQSjKiwG_qHI",
+    authDomain: "mudepo-2d1e5.firebaseapp.com",
+    projectId: "mudepo-2d1e5",
+    storageBucket: "mudepo-2d1e5.appspot.com",
+    messagingSenderId: "1086752633854",
+    appId: "1:1086752633854:web:2b8e446978d8b3bff686af"
+  };
+  const app = initializeApp(firebaseConfig);
+  
   return (
     <html lang="en">
       <body className={inter.className}>
