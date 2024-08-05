1daaaee1257f980ca7f8559409f92c81264a171d
Sergio Bejarano <sergio@wilbby.com>
2024-08-05 17:44:35 +0200

    axios adapter

---
+++ app/config/axios.adapter.ts
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+const axiosInstance = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  timeout: 5000,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+export const axiosAdapter = {
+  fetchData: async (url: string, options = {}) => {
+    try {
+      const response = await axiosInstance(url, options);
+      const data = response.data.json();
+      return data;
+    } catch (error) {
+      console.error("Error retrieving data:", error);
+      throw new Error("Could not get data");
+    }
+  },
+};
