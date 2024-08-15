a8bdab2534ecb4f2e112c1cd743815904747bf06
Sergio Bejarano <sergio@wilbby.com>
2024-08-15 11:58:48 +0200

    close modal click outside

--- app/components/EventContent/Calendar/index.tsx
+++ app/components/EventContent/Calendar/index.tsx
@@ -1232,7 +1232,14 @@ const Calendar = ({ data }: any) => {
         ))
       )}
       {selectedInfo && (
-        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-10">
+        <div
+          className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-10"
+          onClick={(e) => {
+            if (e.target === e.currentTarget) {
+              setSelectedInfo(null);
+            }
+          }}
+        >
           <div className="bg-white p-6 rounded-md min-w-[20%]">
             <h3 className="text-xl font-bold mb-4">INFORMACIÓN DEL PARTIDO</h3>
             <div className="border-t border-gray-300 my-1" />
