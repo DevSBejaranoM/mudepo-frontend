31f5573033a48f99f8178acc6870f29de701c114
Sergio Bejarano <sergio@wilbby.com>
2024-08-15 10:15:41 +0200

    calendar section v0.9

--- app/components/EventContent/CustomTable/index.tsx
+++ app/components/EventContent/CustomTable/index.tsx
@@ -169,6 +169,7 @@ const CustomTable = ({
         const [phaseSelected, setPaheSelected] = useState<any>(0);
         const [groupSelected, setGroupSelected] = useState<any>(0);
         const [dataFiltered, setDataFiltered] = useState<any>(null);
+
         useEffect(() => {
           let newData =
             data?.fases[phaseSelected]?.grupos[groupSelected]?.partidos;
@@ -189,23 +190,42 @@ const CustomTable = ({
           }
         };
 
-        const handleChangePhase = (value: any) => {
+        const handleChangePhase = (value: any, idSelect: string) => {
           setPaheSelected(value);
           let newData = data?.fases[value]?.grupos[groupSelected]?.partidos;
           newData = orderData(newData);
           setDataFiltered(newData);
+          openListAndClose(idSelect);
         };
 
-        const handleChangeGroup = (value: any) => {
+        const handleChangeGroup = (value: any, idSelect: string) => {
           setGroupSelected(value);
           let newData = data?.fases[phaseSelected]?.grupos[value]?.partidos;
           newData = orderData(newData);
           setDataFiltered(newData);
+          openListAndClose(idSelect);
         };
 
-        console.log("phaseSelected", phaseSelected);
-        console.log("groupSelected", groupSelected);
-        console.log("dataFiltered", dataFiltered);
+        const openListAndClose = (id: string) => {
+          const list = document.getElementById(id);
+          const lists = document.querySelectorAll("[role='listbox']");
+          lists.forEach((list) => {
+            if (list.id !== id) {
+              list.classList.replace("block", "hidden");
+            }
+          });
+          if (list && list.classList.contains("hidden")) {
+            list.classList.replace("hidden", "block");
+          } else if (list && list.classList.contains("block")) {
+            list.classList.replace("block", "hidden");
+          }
+        };
+
+        const numberToLetter = (num: number)=> {
+          const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+          return alphabet[num] || "";
+      }
+      
 
         return (
           <>
@@ -222,37 +242,121 @@ const CustomTable = ({
                     Jornada {journey}
                   </h2>
                   <div className="flex space-x-2 w-full sm:w-auto">
-                    <select
-                      id="fase-select"
-                      className="w-full sm:w-auto p-2 border border-gray-300 rounded"
-                      onChange={(e) => handleChangePhase(e.target.value)}
-                    >
-                      {
-                        data?.fases.map((fase: any, index: number) => (
-                          <option key={index} value={index}>
-                            {fase.name}
-                          </option>
-                        ))
-                      }
-                    </select>
-                    <select
-                      id="grupo-select"
-                      className="w-full sm:w-auto p-2 border border-gray-300 rounded"
-                      onChange={(e) => handleChangeGroup(e.target.value)}
-                    >
-                      {
-                        data?.fases[phaseSelected]?.grupos.map((grupo: any, index: number) => (
-                          <option key={index} value={index}>
-                            {grupo.name}
-                          </option>
-                        ))
-                      }
-                    </select>
+                    <div className="w-full">
+                      <div className="relative mt-2">
+                        <button
+                          type="button"
+                          className="relative w-full rounded-md py-2 pl-3 pr-10 color-primary shadow-sm ring-1 ring-inset focus:outline-none focus:ring-2 sm:text-sm sm:leading-6 border-primary hover-border-primary-dark px-4 text-center"
+                          aria-haspopup="listbox"
+                          aria-expanded="true"
+                          aria-labelledby="listbox-fase-label"
+                          onClick={() =>
+                            openListAndClose("listbox-fase-option")
+                          }
+                        >
+                          <span className="flex items-center justify-center">
+                            <span className="ml-3 block truncate">
+                              Fase {phaseSelected + 1}
+                            </span>
+                          </span>
+                          <span className="pointer-events-none absolute inset-y-0 right-0 ml-3 flex items-center pr-2">
+                            <svg
+                              className="h-5 w-5 text-gray-400"
+                              viewBox="0 0 20 20"
+                              fill="currentColor"
+                              aria-hidden="true"
+                            >
+                              <path
+                                fill-rule="evenodd"
+                                d="M10 3a.75.75 0 01.55.24l3.25 3.5a.75.75 0 11-1.1 1.02L10 4.852 7.3 7.76a.75.75 0 01-1.1-1.02l3.25-3.5A.75.75 0 0110 3zm-3.76 9.2a.75.75 0 011.06.04l2.7 2.908 2.7-2.908a.75.75 0 111.1 1.02l-3.25 3.5a.75.75 0 01-1.1 0l-3.25-3.5a.75.75 0 01.04-1.06z"
+                                clip-rule="evenodd"
+                              />
+                            </svg>
+                          </span>
+                        </button>
+                        <ul
+                          className="absolute z-10 mt-1 max-h-56 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm hidden"
+                          role="listbox"
+                          id="listbox-fase-option"
+                          aria-labelledby="listbox-fase-label"
+                          aria-activedescendant="listbox-fase-option-3"
+                        >
+                          {data?.fases.map((fase: any, index: number) => (
+                            <li
+                              key={index}
+                              className="relative select-none py-2 pl-3 pr-9 text-gray-900 cursor-pointer hover:bg-gray-300"
+                              role="option"
+                              onClick={() =>
+                                handleChangePhase(index, "listbox-fase-option")
+                              }
+                            >
+                              {fase.name}
+                            </li>
+                          ))}
+                        </ul>
+                      </div>
+                    </div>
+                    <div className="w-full">
+                      <div className="relative mt-2">
+                        <button
+                          type="button"
+                          className="relative w-full rounded-md py-2 pl-3 pr-10 color-primary shadow-sm ring-1 ring-inset focus:outline-none focus:ring-2 sm:text-sm sm:leading-6 border-primary hover-border-primary-dark px-4 text-center"
+                          aria-haspopup="listbox"
+                          aria-expanded="true"
+                          aria-labelledby="listbox-grupo-label"
+                          onClick={() =>
+                            openListAndClose("listbox-grupo-option")
+                          }
+                        >
+                          <span className="flex items-center justify-center">
+                            <span className="ml-3 block truncate">
+                              Grupo {numberToLetter(groupSelected)}
+                            </span>
+                          </span>
+                          <span className="pointer-events-none absolute inset-y-0 right-0 ml-3 flex items-center pr-2">
+                            <svg
+                              className="h-5 w-5 text-gray-400"
+                              viewBox="0 0 20 20"
+                              fill="currentColor"
+                              aria-hidden="true"
+                            >
+                              <path
+                                fill-rule="evenodd"
+                                d="M10 3a.75.75 0 01.55.24l3.25 3.5a.75.75 0 11-1.1 1.02L10 4.852 7.3 7.76a.75.75 0 01-1.1-1.02l3.25-3.5A.75.75 0 0110 3zm-3.76 9.2a.75.75 0 011.06.04l2.7 2.908 2.7-2.908a.75.75 0 111.1 1.02l-3.25 3.5a.75.75 0 01-1.1 0l-3.25-3.5a.75.75 0 01.04-1.06z"
+                                clip-rule="evenodd"
+                              />
+                            </svg>
+                          </span>
+                        </button>
+                        <ul
+                          className="absolute z-10 mt-1 max-h-56 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm hidden"
+                          role="listbox"
+                          id="listbox-grupo-option"
+                          aria-labelledby="listbox-grupo-label"
+                          aria-activedescendant="listbox-grupo-option-3"
+                        >
+                          {
+                            data?.fases[phaseSelected]?.grupos.map(
+                              (grupo: any, index: number) => (
+                                <li
+                                  key={index}
+                                  className="relative select-none py-2 pl-3 pr-9 text-gray-900 cursor-pointer hover:bg-gray-300"
+                                  role="option"
+                                  onClick={() =>
+                                    handleChangeGroup(index, "listbox-grupo-option")
+                                  }
+                                >
+                                  {grupo.name}
+                                </li>
+                              )
+                            )
+                          }
+                        </ul>
+                      </div>
+                    </div>
                   </div>
                 </div>
-                {/* <h2 className="text-xl font-bold mb-4 text-red-600">
-                  Jornada {journey}
-                </h2> */}
+
                 <div className="overflow-x-auto">
                   <table className="min-w-full bg-gray-300">
                     <thead>
