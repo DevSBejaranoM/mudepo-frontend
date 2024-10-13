94dbd4228bbcdb5438fc7db361c79bdec00e9071
Sergio Bejarano Martín <dev.sbejaranom@gmail.com>
2024-10-13 20:15:03 +0200

    timeout

--- app/config/axios.adapter.ts
+++ app/config/axios.adapter.ts
@@ -1,8 +1,9 @@
 import axios from "axios";
 
+// se aumentan los tiempos de espera para las peticiones hasta que se resuelva el problema de la lentitud
 const axiosInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
-  timeout: 15000,
+  timeout: 150000,
   headers: {
     "Content-Type": "application/json",
   },
